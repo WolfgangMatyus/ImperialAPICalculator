2f22579bc612e9d7c40c197d75907dfa8f49e386
Matyus Wolfgang <wi21b055@technikum-wien.at>
2022-06-22 17:41:31 +0200

    Initial Commit

---
+++ src/test/java/at/msw/imperialapicalculator/ImperialApiCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.msw.imperialapicalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperialApiCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
